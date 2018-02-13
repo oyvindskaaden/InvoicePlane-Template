4f59c2f63b99df513a5b151e9970ca1afd643d78
Øyvind Skaaden <oyvindskaaden@gmail.com>
2018-02-13 13:03:40 +0100

    Found bug with newest version

--- mpdf_helper.php
+++ mpdf_helper.php
@@ -1,29 +1,25 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
-
 /*
  * InvoicePlane
  *
  * @author		InvoicePlane Developers & Contributors
- * @copyright	Copyright (c) 2012 - 2018 InvoicePlane.com
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
  */
-
 /**
  * Create a PDF
  *
  * @param $html
- * @param $filename
+ * @param string $filename
  * @param bool $stream
  * @param null $password
- * @param null $isInvoice
+ * @param boolean $isInvoice
  * @param null $is_guest
  * @param bool $zugferd_invoice
  * @param null $associated_files
- *
  * @return string
- * @throws \Mpdf\MpdfException
  */
 function pdf_create(
     $html,
@@ -36,16 +32,13 @@ function pdf_create(
     $associated_files = null
 ) {
     $CI = &get_instance();
-
     // Get the invoice from the archive if available
     $invoice_array = array();
-
     // mPDF loading
     if (!defined('_MPDF_TEMP_PATH')) {
-        define('_MPDF_TEMP_PATH', UPLOADS_TEMP_MPDF_FOLDER);
-        define('_MPDF_TTFONTDATAPATH', UPLOADS_TEMP_MPDF_FOLDER);
+        define('_MPDF_TEMP_PATH', FCPATH . 'uploads/temp/mpdf/');
+        define('_MPDF_TTFONTDATAPATH', FCPATH . 'uploads/temp/mpdf/');
     }
-
     $mpdf = new \Mpdf\Mpdf(['format' => 'A4']);
 
     // mPDF configuration
@@ -54,12 +47,11 @@ function pdf_create(
     $mpdf->setAutoTopMargin = true;
     $mpdf->setAutoBottomMargin = true;
     $mpdf->use_kwt = true;
-
+    
     if (IP_DEBUG) {
         // Enable image error logging
         $mpdf->showImageErrors = true;
     }
-
     // Include zugferd if enabled
     if ($zugferd_invoice) {
         $CI->load->helper('zugferd');
@@ -68,68 +60,64 @@ function pdf_create(
         $mpdf->SetAdditionalXmpRdf(zugferd_rdf());
         $mpdf->SetAssociatedFiles($associated_files);
     }
-
     // Set a password if set for the voucher
     if (!empty($password)) {
         $mpdf->SetProtection(array('copy', 'print'), $password, $password);
     }
-
     // Check if the archive folder is available
-    if (!(is_dir(UPLOADS_ARCHIVE_FOLDER) || is_link(UPLOADS_ARCHIVE_FOLDER))) {
-        mkdir(UPLOADS_ARCHIVE_FOLDER, '0777');
+    if (!(is_dir('./uploads/archive/') || is_link('./uploads/archive/'))) {
+        mkdir('./uploads/archive/', '0777');
     }
-
     // Set the footer if voucher is invoice and if set in settings
-    if (!empty($CI->mdl_settings->settings['pdf_invoice_footer']) && $isInvoice) {
+    if (!empty($CI->mdl_settings->settings['pdf_invoice_footer'])) {
         $mpdf->setAutoBottomMargin = 'stretch';
         $mpdf->SetHTMLFooter('<div id="footer">' . $CI->mdl_settings->settings['pdf_invoice_footer'] . '</div>');
     }
-
-    // Set the footer if voucher is quote and if set in settings
-    if (!empty($CI->mdl_settings->settings['pdf_quote_footer']) && strpos($filename, trans('quote')) !== false) {
-        $mpdf->setAutoBottomMargin = 'stretch';
-        $mpdf->SetHTMLFooter('<div id="footer">' . $CI->mdl_settings->settings['pdf_quote_footer'] . '</div>');
-    }
-
     // Watermark
     if (get_setting('pdf_watermark')) {
         $mpdf->showWatermarkText = true;
     }
-
-    $mpdf->WriteHTML((string) $html);
-
+    $mpdf->WriteHTML($html);
     if ($isInvoice) {
-
-        foreach (glob(UPLOADS_ARCHIVE_FOLDER . '*' . $filename . '.pdf') as $file) {
+        foreach (glob(UPLOADS_FOLDER . 'archive/*' . $filename . '.pdf') as $file) {
             array_push($invoice_array, $file);
         }
-
         if (!empty($invoice_array) && !is_null($is_guest)) {
             rsort($invoice_array);
-
             if ($stream) {
                 return $mpdf->Output($filename . '.pdf', 'I');
             } else {
                 return $invoice_array[0];
             }
         }
-
-        $archived_file = UPLOADS_ARCHIVE_FOLDER . date('Y-m-d') . '_' . $filename . '.pdf';
+        $archived_file = UPLOADS_FOLDER . 'archive/' . date('Y-m-d') . '_' . $filename . '.pdf';
         $mpdf->Output($archived_file, 'F');
-
         if ($stream) {
             return $mpdf->Output($filename . '.pdf', 'I');
         } else {
             return $archived_file;
         }
     }
-
     // If $stream is true (default) the PDF will be displayed directly in the browser
     // otherwise will be returned as a download
     if ($stream) {
         return $mpdf->Output($filename . '.pdf', 'I');
     } else {
-        $mpdf->Output(UPLOADS_TEMP_FOLDER . $filename . '.pdf', 'F');
-        return UPLOADS_TEMP_FOLDER . $filename . '.pdf';
+        $mpdf->Output(UPLOADS_FOLDER . 'temp/' . $filename . '.pdf', 'F');
+        // Housekeeping
+        // Delete any files in temp/ directory that are >1 hrs old
+        $interval = 3600;
+        if ($handle = @opendir(preg_replace('/\/$/', '', './uploads/temp/'))) {
+            while (false !== ($file = readdir($handle))) {
+                if (
+                    $file && !in_array($file, ['..', '.', 'remove.txt'])
+                    && !is_dir($file) && (substr($file, 0, 1) !== '.')
+                    && (filemtime('./uploads/temp/' . $file) + $interval) < time()) {
+                        unlink(UPLOADS_FOLDER . 'temp/' . $file);
+                }
+            }
+            closedir($handle);
+        }
+        return UPLOADS_FOLDER . 'temp/' . $filename . '.pdf';
     }
 }
